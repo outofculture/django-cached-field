02c95bc18e0ed5d65d9063c07ef45d6cc7a43610
Martin Chase <outofculture@gmail.com>
2015-01-20 16:07:41 -0800

    whitespace, todo, str format

--- django_cached_field/tasks.py
+++ django_cached_field/tasks.py
@@ -5,17 +5,21 @@ import re
 
 
 logger = get_task_logger(__name__)
+# TODO 20150119: this needs to handle custom named recalculation fields
 recalc_needed_re = re.compile("(.*)_recalculation_needed$")
 
 @shared_task
 def offload_cache_recalculation(app, model, obj_id, **kwargs):
-    model = get_model(app,model)
+    model = get_model(app, model)
     try:
         obj = model.objects.get(pk=obj_id)
         for f in model._meta.fields:
             match = recalc_needed_re.search(f.name)
             if match and getattr(obj, f.name):
                 basename = match.groups()[0]
-                getattr(obj, "recalculate_%s" % basename)()
+                getattr(obj, "recalculate_{0:s}".format(basename))()
     except model.DoesNotExist:
-        logger.warning('%s.%s with pk %s does not exist.  Was offload_cache_recalculation called before initial object creation or after object deletion?' % (app, model, obj_id))
+        logger.warning(
+            ('{0:s}.{1:s} with pk {2:s} does not exist.  Was offload_cache_recalculation '
+             + 'called before initial object creation or after object deletion?').format(
+                app, model, obj_id))
